9f44725a8cefe0ba5083fef059912c9d09ad7a10
Tomas Pastrana <63085@sistemas.frc.utn.edu.ar>
2022-05-27 15:41:20 -0300

    ADD: new consumer screen added alternative card grid component

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import React from "react";
+import Consumer from "../pages/consumer/Consumer";
 import Home from "../pages/home/Home";
 import MyStreams from "../pages/mystreams/Mystreams";
 import Profile from "../pages/profile/Profile";
@@ -8,6 +9,7 @@ export const AppRouter = (): JSX.Element => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/consumer" element={<Consumer />} />
             <Route path="/mystreams" element={<MyStreams />} />
             <Route path="/profile" element={<Profile />} />
         </Routes>
